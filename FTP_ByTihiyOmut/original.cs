0a8391b2076f45f6ebed362c1fc40cac78fb4873
x <136571437+d3fpy@users.noreply.github.com>
2026-06-25 00:48:06 +0300

    Update original.cs

--- FTP_ByTihiyOmut/original.cs
+++ FTP_ByTihiyOmut/original.cs
@@ -1,12 +1,19 @@
 // Password.OhLookUFoundPasswordYesItsASecretCommentTihiyOmutEZCrack
-// Token: 0x06000001 RID: 1
+// Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
 private static void Main()
 {
 	Console.WriteLine("Find the password!");
 	Console.WriteLine("Made by TihiyOmut");
 	Console.WriteLine("Made on .NET, you must use dnSpy or like that\n");
 	Console.Write("Enter password: ");
-	Console.WriteLine("\n good boy! You found the password and its correct");
+	if (Console.ReadLine() == "TihiyOmut_Secret_2026")
+	{
+		Console.WriteLine("\n good boy! You found the password and its correct");
+	}
+	else
+	{
+		Console.WriteLine("\n bro r u serious? You cant even find the password?");
+	}
 	Console.WriteLine("\nPress Enter to exit...");
 	Console.ReadLine();
 }
